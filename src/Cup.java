253959cf883132d91daf28aaece761fcb1017bf1
gitgud <jonathanbirkstrom@gmail.com>
2025-03-12 10:02:57 +0100

    j

--- src/Cup.java
+++ src/Cup.java
@@ -1,2 +1,50 @@
 public class Cup {
+
+    private String drinkType;
+    private Size size;
+    private boolean hasWhippedCream;
+
+    public Cup(String drinkType, Size size, boolean hasWhippedCream) {
+        this.drinkType = drinkType;
+        this.size = size;
+        this.hasWhippedCream = hasWhippedCream;
+    }
+
+    public Cup(String drinkType, Size size) {
+        this.drinkType = drinkType;
+        this.size = size;
+    }
+
+    public String getDrinkType() {
+        return drinkType;
+    }
+
+    public void setDrinkType(String drinkType) {
+        this.drinkType = drinkType;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public boolean isHasWhippedCream() {
+        return hasWhippedCream;
+    }
+
+    public void setHasWhippedCream(boolean hasWhippedCream) {
+        this.hasWhippedCream = hasWhippedCream;
+    }
+
+    @Override
+    public String toString() {
+        return "Cup{" +
+                "drinkType='" + drinkType + '\'' +
+                ", size=" + size +
+                ", hasWhippedCream=" + hasWhippedCream +
+                '}';
+    }
 }
